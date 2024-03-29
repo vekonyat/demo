6fe392d1623cda5276ac58c4c9e1c319b9d09c03
veko <vekonyat@gmail.com>
2024-03-29 13:15:22 +0100

    Added newrace HTML, modified controller

--- src/main/java/com/example/demo/HellRunController.java
+++ src/main/java/com/example/demo/HellRunController.java
@@ -2,10 +2,10 @@ package com.example.demo;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,7 +34,7 @@ public class HellRunController {
     }
 
     @GetMapping("/")
-    public String hellrace() {
+    public String hellRace() {
         return "hellrace";
     }
     @GetMapping("/getRaceRunners/{id}")
@@ -43,4 +43,25 @@ public class HellRunController {
         model.addAttribute("runners", runner);
         return "runners";
     }
+
+    @GetMapping("/newrace")
+    public String getNewRace(Model model) {
+        model.addAttribute("raceData", new HellRaceEntity());
+        return "newrace";
+    }
+
+    @PostMapping("/addnewrace")
+    public String addRace(@ModelAttribute HellRaceEntity raceData) {
+        HellRaceEntity newRace = new HellRaceEntity();
+
+        newRace.setRaceName(raceData.getRaceName());
+        newRace.setRaceKm(raceData.getRaceKm());
+ //      newResult.setRace(hellraceRepository.findById(resultData.getRaceId()).orElse(null));
+  //      newResult.setRunner(hellrunnerRepository.findById(resultData.getRunnerId()).orElse(null));
+        hellraceRepository.save(newRace);
+        return "redirect:/races";
+
+    }
+
+
 }
